332d17b59ec6414963fae1176690d9ea904d5c21
Javier Reyes <jreyes@portalfinance.co>
2019-06-07 14:21:15 -0500

    Adding styles to table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,23 +1,21 @@
 import * as React from 'react';
 
-// // Definiendo estilos para Input
-// const style = {
-//  backgroundColor: '#fff',
-//  border: '1px solid #eee',
-//  borderRadius: '4px',
-//  marginBottom: '10px',
-//  padding: '10px 15px',
-//  width: 'calc(100% - 30px)',
-// }
-
-// // Definiendo estilos para el span
-// const spanStyle = {
-//  color: '#777',
-//  fontSize: '10px',
-//  fontWeight: 900,
-//  textTransform: 'uppercase'
-// } as React.CSSProperties // Se indica a react que lo tome como propiedades Css, no como un objeto
-
+// Definiendo estilos para Input
+const styleTable = {
+  cell: {
+    border: '1px solid black',
+  },
+  row: {
+    border: '1px solid black',
+  },
+  table: {
+    border: '1px solid black',
+    borderCollapse: 'collapse',
+    marginBottom: '40px',
+    textAlign: 'center'
+  } as React.CSSProperties,
+  
+} 
 interface ITableProps {
  array: []
 }
@@ -25,22 +23,23 @@ interface ITableProps {
 const Table: React.StatelessComponent<ITableProps> = props => {
  const { array } = props
  return (
-  <table>
+  
+  <table style={styleTable.table}>
       <thead>
         <tr>
-          <th>Código</th>
-          <th>Nombre</th>
-          <th>Código Padre</th>
-          <th>Descripción Padre</th>
+          <th style={styleTable.cell}>Código</th>
+          <th style={styleTable.cell}>Nombre</th>
+          <th style={styleTable.cell}>Código Padre</th>
+          <th style={styleTable.cell}>Descripción Padre</th>
         </tr>
       </thead>
       <tbody>
       {array ? array.map((d: any) => 
-        <tr key={d.cod}>
-          <td>{d.cod}</td>
-          <td>{d.nombre}</td>
-          <td>{d.codPa ? d.codPa : '-'}</td>
-          <td>{d.nomPadre ? d.nomPadre : '-'}</td>
+        <tr key={d.cod} style={styleTable.row}>
+          <td style={styleTable.cell}>{d.cod}</td>
+          <td style={styleTable.cell}>{d.nombre}</td>
+          <td style={styleTable.cell}>{d.codPa ? d.codPa : '-'}</td>
+          <td style={styleTable.cell}>{d.nomPadre ? d.nomPadre : '-'}</td>
         </tr>
       ) : ''}
       </tbody>
